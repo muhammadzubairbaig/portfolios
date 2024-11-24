d4a2525f2a52329fa7ed8b8df24ea6c2b7381563
Muhammad Zubair Baig <zubair@autoleap.com>
2024-11-24 16:10:14 +0100

    sss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, // Set limit to 1MB or any size you prefer
     rollupOptions: {
       output: {
         manualChunks: {
