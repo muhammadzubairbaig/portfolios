9c6ac0eb6b708ebefd4b7c2a2a36f3b4d43eed42
Muhammad Zubair Baig <zubair@autoleap.com>
2024-11-24 16:14:09 +0100

    w

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Set limit to 1MB or any size you prefer
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'some-large-library'],
+          vendor: ['react', 'react-dom'],
         },
       },
     },
